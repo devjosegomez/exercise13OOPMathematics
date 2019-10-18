1657c2673f000b658df6dda7253034f5c37f819d
devjosegomez <devjosegomez@gmail.com>
2019-10-18 13:18:11 -0500

    Added Mathematics app

---
+++ src/com/exercise14oopmathematics/app/MathematicsApp.java
@@ -0,0 +1,19 @@
+package com.exercise14oopmathematics.app;
+
+import java.util.Scanner;
+
+public class MathematicsApp {
+
+	public static void main(String[] args) {
+		//variables declaration
+		double base = 0;
+		double height = 0;
+		double radio = 0;
+		double side = 0;
+		
+		Scanner input = new Scanner(System.in);
+		
+
+	}
+
+}
