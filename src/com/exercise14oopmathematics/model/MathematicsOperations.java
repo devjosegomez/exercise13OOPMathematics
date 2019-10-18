df9dca2aa706419961f1764ca713ad15393203a1
devjosegomez <devjosegomez@gmail.com>
2019-10-18 13:16:28 -0500

    Added methods AreaTriangle(), areaSquare(), areaCircle()

--- src/com/exercise14oopmathematics/model/MathematicsOperations.java
+++ src/com/exercise14oopmathematics/model/MathematicsOperations.java
@@ -1,5 +1,19 @@
 package com.exercise14oopmathematics.model;
 
 public class MathematicsOperations {
+	final double PI= 3.141516;
+	public double areaTriangle(double base, double height) {
+		return (base*height)/2;
+	}
+	
+	public double areaSquare(double side) {
+		return (side*side);
+	}
+	
+	public double areaCircle(double radio) {
+		return this.PI*(radio*radio);
+	}
+	
+	
 	
 }
